a6af8a7b1113938220805a52415ced85ca211ee8
Vincent <vincent@windows97.space>
2025-03-09 14:25:34 -0400

    I just discovered the atexit function

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "metalloc.h"
 
-int main(int argc, char *argv[])
-{
-	char *c;
+int main(int argc, char *argv[]) {
+	if (atexit(delall) != 0)
+		exit(1);
 
-	c = new(9);
+	char *c = new(9);
 	printf("%d\n%d\n", len(c), cap(c));
-
-	delall();
 }
